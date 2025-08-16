3d1fb2e4c1f5a8f1af8d567ceae073acc8a85ec3
Ica <2206041@itg.ac.id>
2025-08-16 10:28:35 +0700

    feat: implement quiz feature for siswa

--- database/migrations/2025_08_16_031608_create_quiz_attempts_table.php
+++ database/migrations/2025_08_16_031608_create_quiz_attempts_table.php
@@ -11,8 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('siswa_answers', function (Blueprint $table) {
+        Schema::create('quiz_attempts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('quiz_id')->constrained('quizzes')->onDelete('cascade');
+            $table->integer('skor')->nullable();
             $table->timestamps();
         });
     }
@@ -22,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('siswa_answers');
+        Schema::dropIfExists('quiz_attempts');
     }
 };
